cdf27f806fa3c04afcee9eeb4230af087fcb4bca
tgotic <tgotic@users.sourceforge.net>
2011-01-17 19:36:41 +0100

    updated version to 2.4.1

--- backend/dllversion.c
+++ backend/dllversion.c
@@ -1,5 +1,5 @@
 /* Sed: http://msdn.microsoft.com/library/en-us/shellcc/platform/shell/programmersguide/versions.asp */
-#if defined (_WIN32) && (defined(_USRDLL) || defined(DLL_EXPORT) || defined(PIC))
+#if defined (WIN32) && (defined(_USRDLL) || defined(DLL_EXPORT) || defined(PIC))
 #include <windows.h>
 #include <shlwapi.h>
 
@@ -20,12 +20,12 @@ HRESULT DllGetVersion (DLLVERSIONINFO2* pdvi)
 		return (E_INVALIDARG);
 		
 	pdvi->info1.dwMajorVersion = 2;
-	pdvi->info1.dwMinorVersion = 2;
+	pdvi->info1.dwMinorVersion = 4;
 	pdvi->info1.dwBuildNumber = 1;
 	pdvi->info1.dwPlatformID = DLLVER_PLATFORM_WINDOWS;
 	if (sizeof(DLLVERSIONINFO2) == pdvi->info1.cbSize)
-		pdvi->ullVersion = MAKEDLLVERULL(2, 2, 1, 0);
+		pdvi->ullVersion = MAKEDLLVERULL(2, 4, 1, 0);
 	
 	return S_OK;
 }
-#endif /* _WIN32 */
+#endif /* WIN32 */
